0cce13eeba6e9d96dc1cd7943f29ef7a2afcba98
phiem <phiem.huynh@gmail.com>
2025-07-19 23:57:36 -0400

    clean up

--- hello.py
+++ hello.py
@@ -2,30 +2,32 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
+# https://docs.streamlit.io/get-started/fundamentals/main-concepts
 
-df = pd.DataFrame({
-  'first column': [1, 2, 3, 4],
-  'second column': [10, 20, 30, 40]
-})
-
-df
-
-st.write("Here's our first attempt at using data to create a table:")
+st.write("DataFrame Example:")
 st.write(pd.DataFrame({
     'first column': [1, 2, 3, 4],
     'second column': [10, 20, 30, 40]
 }))
-st.write("Numpy")
-dataframe = np.random.randn(10, 20)
-st.dataframe(dataframe)
 st.divider()
-st.write("Numpy DataFrame")
+st.write("Numpy Random DataFrame w. Highlight Example:")
 dataframe = pd.DataFrame(
     np.random.randn(10, 20),
     columns=('header %d' % i for i in range(20)))
-
 st.dataframe(dataframe.style.highlight_max(axis=0)) #column-wise max
-
 st.dataframe(dataframe.style.highlight_max(axis=1)) #row-wise max
-st.table(dataframe)
 
+st.divider()
+import time
+st.write("Progress Bar Example:")
+# Add a placeholder
+latest_iteration = st.empty()
+bar = st.progress(0)
+
+for i in range(100):
+  # Update the progress bar with each iteration.
+  latest_iteration.text(f'Iteration {i+1}')
+  bar.progress(i + 1)
+  time.sleep(0.1)
+
+'...and now we\'re done!'
